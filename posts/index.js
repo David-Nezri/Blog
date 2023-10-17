8a947851e475b89eaf66bad35905fb1ef9a48208
David <davidnezri1990@gmail.com>
2023-10-17 22:51:25 +0300

    create comments in posts service

--- posts/index.js
+++ posts/index.js
@@ -1,3 +1,4 @@
+//imports for service
 const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
@@ -10,19 +11,25 @@ app.use(cors());
 
 const posts = {};
 
+//get request method and create API
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
+//post request method and create API 
 app.post("/posts", async (req, res) => {
+    //create uniq id
   const id = randomBytes(4).toString("hex");
+  //we put all kind of data into a object 
   const { title } = req.body;
 
+  //the post get the uniq id for ech post
   posts[id] = {
     id,
     title,
   };
-
+ 
+  //we using microservices architecture so all the data path thru the event-bus service
   await axios.post("http://localhost:4005/events", {
     type: "PostCreated",
     data: {

d8e488e0bb61ad983fa1c93c991e08190dc00be8
David <davidnezri1990@gmail.com>
2023-10-18 23:17:14 +0300

    update url

--- posts/index.js
+++ posts/index.js
@@ -1,4 +1,3 @@
-//imports for service
 const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
@@ -11,26 +10,20 @@ app.use(cors());
 
 const posts = {};
 
-//get request method and create API
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-//post request method and create API 
-app.post("/posts", async (req, res) => {
-    //create uniq id
-  const id = randomBytes(4).toString("hex");
-  //we put all kind of data into a object 
+app.post('/posts/create', async (req, res) => {
+  const id = randomBytes(4).toString('hex');
   const { title } = req.body;
 
-  //the post get the uniq id for ech post
   posts[id] = {
     id,
     title,
   };
- 
-  //we using microservices architecture so all the data path thru the event-bus service
-  await axios.post("http://localhost:4005/events", {
+
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: {
       id,
